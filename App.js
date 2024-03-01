2684fe89877d86561b7a1fbd1b8d0d3b8ba93936
MartinBernal06 <78180863+MartinBernal06@users.noreply.github.com>
2024-03-01 16:06:57 -0700

    menu y nabvar

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import React from 'react'
-import Main from './src/components/Main'
 import { NativeRouter } from 'react-router-native'
+import BottomTabs from './src/components/Bottom-Tabs'
+import Header from './src/components/Header-App'
+import { View } from 'react-native-web'
 
 export default function App() {
-  return <NativeRouter><Main /></NativeRouter>
+  return (
+    <NativeRouter>
+      <View style={{ flex: 1 }}>
+        <Header />
+        <BottomTabs />
+      </View>
+    </NativeRouter>
+  )
 }

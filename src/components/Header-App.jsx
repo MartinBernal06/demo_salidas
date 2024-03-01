c7534394d30cd62566897e591b7e8bdd4bb0940b
MartinBernal06 <78180863+MartinBernal06@users.noreply.github.com>
2024-03-01 16:40:26 -0700

    icono del logo

--- src/components/Header-App.jsx
+++ src/components/Header-App.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                 <MaterialCommunityIcons name="backburger" color="#F05A28" size={30} />
             </View>
             <View style={[styles.column2, styles.centerColumn]}>
-                <MaterialCommunityIcons name="ticket-confirmation-outline" color="black" size={30} />
+                <Image source={require('../../assets/logo200x66.png')} style={styles.image} />
             </View>
             <View style={styles.column3}>
                 <MaterialCommunityIcons name="bell-badge-outline" color="#F05A28" size={30} />
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
     column: {
         flex: 0.2, // Ocupa el 20% del ancho total
         alignItems: 'center',
+        top: -10
     },
     column2: {
         flex: 0.6, // Ocupa el 20% del ancho total
@@ -37,14 +38,16 @@ const styles = StyleSheet.create({
     column3: {
         flex: 0.2, // Ocupa el 20% del ancho total
         alignItems: 'center',
+        top: -10
     },
     centerColumn: {
         alignItems: 'center', // Centra el contenido en el segundo contenedor
     },
     image: {
-        width: '80%', // Tamaño de las imágenes
-        height: '80%', // Tamaño de las imágenes
-        resizeMode: 'contain', // Ajustar imagen dentro del contenedor
+        width: '100%', // Tamaño de las imágenes
+        height: '100%', // Tamaño de las imágenes
+        resizeMode: 'contain',
+        top: -20 // Ajustar imagen dentro del contenedor
     },
 });
 
